9f89f959430a833b6b6651dc3a8e4bef3ca136e0
Rodney Lorrimar <dev@rodney.id.au>
2016-11-30 23:44:36 +0000

    More hawk tests

--- test/Network/Hawk/Tests.hs
+++ test/Network/Hawk/Tests.hs
@@ -5,9 +5,11 @@ module Network.Hawk.Tests (tests) where
 import Data.Either (isRight)
 import Data.Default
 import Data.ByteString (ByteString)
+import qualified Data.ByteString.Lazy as BL
 import Network.Wai (Request(..), defaultRequest)
 import Network.HTTP.Client (Response(..))
 import Network.HTTP.Client.Internal (Response(..))
+import Network.HTTP.Types (Method, RequestHeaders)
 import Network.HTTP.Types.Status (ok200)
 import Data.Text.Encoding (decodeUtf8)
 
@@ -25,8 +27,9 @@ import qualified Network.Hawk.Server.Types as Server (HeaderArtifacts(..))
 tests :: TestTree
 tests = testGroup "Network.Hawk"
         [ testCase "generates a header then successfully parses it" test01
-        --, testCase "generates a header then successfully parses it (WAI request)" test02
+        , testCase "generates a header then successfully parses it (WAI request)" test02
         , testCase "generates a header then successfully parses it (absolute request uri)" test03
+        , testCase "generates a header then fails to parse it (missing server header hash)" test04
         ]
 
 makeCreds :: Client.ClientId -> (Client.Credentials, Server.CredentialsFunc IO String)
@@ -65,16 +68,9 @@ test02 = do
   hdr <- Client.header "http://example.com:8080/resource/4?filter=a" "POST" creds (Just payload) ext
 
   -- Server verifies client request
-  let req = defaultRequest
-        { requestMethod = "POST"
-        , rawPathInfo = "/resource/4"
-        , rawQueryString = "?filter=a"
-        , requestHeaders = [("host", "example.com:8080"),
-                            ("content-type", "text/plain;x=y"),
-                            ("authorization", Client.hdrField hdr)]
-        }
+  let req = mockRequest "POST" "/resource/4" "?filter=a" "example.com:8080" payload [("authorization", Client.hdrField hdr)]
+
   r <- Server.authenticateRequest def credsFunc req (Just (payloadData payload))
-  -- fixme: Left "Bad mac"
   isRight r @? "Expected auth success, got: " ++ show r
   let Right s@(Server.AuthSuccess creds2 arts user) = r
   user @?= "steve"
@@ -84,14 +80,7 @@ test02 = do
   -- Client verifies server response
   let payload2 = PayloadInfo "text/plain" "Some reply"
       hdr2 = Server.header creds2 arts (Just payload2)
-      res = Response
-        { responseStatus = ok200
-        , responseHeaders = [hdr2, ("content-type", payloadContentType payload2)]
-        , responseBody = payloadData payload2
-        , responseVersion = undefined
-        , responseCookieJar = undefined
-        , responseClose' = undefined
-        }
+      res = mockResponse payload2 [hdr2]
       creds2' = clientCreds "" creds2
       arts' = clientHeaderArtifacts arts
   r2 <- Client.authenticate res creds2' arts' (Just (payloadData payload2)) Client.ServerAuthorizationRequired
@@ -119,6 +108,25 @@ clientHeaderArtifacts Server.HeaderArtifacts{..} = Client.HeaderArtifacts
   -- , shaMac       :: ByteString
   }
 
+mockRequest :: Method -> ByteString -> ByteString -> ByteString -> PayloadInfo -> RequestHeaders -> Request
+mockRequest method path qs host (PayloadInfo ct _) hdrs = defaultRequest
+  { requestMethod = method
+  , rawPathInfo = path
+  , rawQueryString = qs
+  , requestHeaderHost = Just host
+  , requestHeaders = [("host", host), ("content-type", ct)] ++ hdrs
+  }
+
+mockResponse :: PayloadInfo -> RequestHeaders -> Response BL.ByteString
+mockResponse (PayloadInfo ct d) hdrs = Response
+        { responseStatus = ok200
+        , responseHeaders = hdrs ++ [("content-type", ct)]
+        , responseBody = d
+        , responseVersion = undefined
+        , responseCookieJar = undefined
+        , responseClose' = undefined
+        }
+
 -- generates a header then successfully parse it (absolute request uri)
 test03 :: Assertion
 test03 = do
@@ -136,7 +144,50 @@ test03 = do
             }
   r <- Server.authenticate def credsFunc hrq
   isRight r @? "Expected auth success, got: " ++ show r
-  let Right s@(Server.AuthSuccess creds' arts user) = r
+  let Right s@(Server.AuthSuccess creds2 arts user) = r
   user @?= "steve"
   Server.shaExt arts @?= Just "some-app-data"
   Server.authenticatePayload s payload @?= Right ()
+
+  let payload2 = PayloadInfo "text/plain" "some reply"
+      fixmeExt2 = "response-specific"
+      hdr = Server.header creds2 arts (Just payload2)
+      res = mockResponse payload2 [hdr]
+      creds2' = clientCreds "" creds2
+      arts' = clientHeaderArtifacts arts
+
+  r2 <- Client.authenticate res creds2' arts' (Just (payloadData payload2)) Client.ServerAuthorizationRequired
+  r2 @?= Right ()
+
+
+-- generates a header then fails to parse it (missing server header hash)
+test04 :: Assertion
+test04 = do
+  let (creds, credsFunc) = makeCreds "123456"
+      ext = Just "some-app-data"
+      payload = PayloadInfo "text/plain;x=y" "some not so random text"
+  hdr <- Client.header "http://example.com:8080/resource/4?filter=a" "POST" creds (Just payload) ext
+  let hrq = def
+            { hrqMethod = "POST"
+            , hrqUrl = "/resource/4?filter=a" -- fixme: not absolute
+            , hrqHost = "example.com"
+            , hrqPort = Just 8080
+            , hrqAuthorization = Client.hdrField hdr
+            , hrqPayload = Nothing
+            }
+  r <- Server.authenticate def credsFunc hrq
+  isRight r @? "Expected auth success, got: " ++ show r
+  let Right s@(Server.AuthSuccess creds2 arts user) = r
+  user @?= "steve"
+  Server.shaExt arts @?= Just "some-app-data"
+  Server.authenticatePayload s payload @?= Right ()
+
+  let payload2 = PayloadInfo "text/plain" "some reply"
+      fixmeExt2 = "response-specific"
+      hdr = Server.header creds2 arts Nothing
+      res = mockResponse payload2 [hdr]
+      creds2' = clientCreds "" creds2
+      arts' = clientHeaderArtifacts arts
+
+  r2 <- Client.authenticate res creds2' arts' (Just (payloadData payload2)) Client.ServerAuthorizationRequired
+  r2 @?= Left "Missing response hash attribute"
